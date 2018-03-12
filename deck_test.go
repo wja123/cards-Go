a47ce585bc2f9670cf3bdf2ad043302f4ff7afd9
wja123 <w.j.alag@gmail.com>
2018-03-11 23:36:39 -0400

    Wrote first test, of deck length

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,13 @@
 package main
 
-func TestNewDeck boolean(){
-	deck := newDeck()
+import "testing"
 
+func TestNewDeck(t *testing.T) {
+  d := newDeck()
+  
+  if len(d) != 52 {
+    t.Errorf("Expected deck length of 52 but got %v",len(d))
+  }
+
+  
 }
