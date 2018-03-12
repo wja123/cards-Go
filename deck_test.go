8cae112129c5b378bca5c164d1058b5d9b476d8f
wja123 <w.j.alag@gmail.com>
2018-03-11 23:55:20 -0400

    Completed write to and retrieval of deck from file

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestNewDeck(t *testing.T) {
   d := newDeck()
-  
+
   if len(d) != 52 {
     t.Errorf("Expected deck length of 52 but got %v",len(d))
   }
@@ -16,6 +19,19 @@ func TestNewDeck(t *testing.T) {
   if d[len(d) - 1] != "King of Diamonds" {
     t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
   }
+}
 
-  
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+ os.Remove("_desktesting")
+
+  d := newDeck()
+  d.saveToFile("_decktesting")
+
+  loadedDeck := newDeckFromFile("_decktesting")
+
+ if len(loadedDeck) != 52 {
+  t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+ }
+
+  os.Remove("_decktesting")
 }
