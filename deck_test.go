e8e0cd79863f7cd16185e19cc4d642843dfe3ce5
wja123 <w.j.alag@gmail.com>
2018-03-11 23:43:19 -0400

    Wrote a test to ensure the first and last cards are correct

--- deck_test.go
+++ deck_test.go
@@ -9,5 +9,13 @@ func TestNewDeck(t *testing.T) {
     t.Errorf("Expected deck length of 52 but got %v",len(d))
   }
 
+  if d[0] != "Ace of Spades" {
+    t.Errorf("Expected first card of Ace of Space, but got %v", d[0])
+  }
+
+  if d[len(d) - 1] != "King of Diamonds" {
+    t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+  }
+
   
 }
